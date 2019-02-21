2d4c33c2d2a1923de504258561020665c86df0d7
Shawn.Wang <wangxcsh@cn.ibm.com>
2019-02-21 15:10:09 +0800

    update

--- src/V_search_Current_Thread.py
+++ src/V_search_Current_Thread.py
@@ -19,19 +19,21 @@ def print_up_stock( stock_code ):
     try:
         # 获取股票实时数据
         df_today = ts.get_realtime_quotes("%06d"%stock_code)
+        # 今日开盘价
+        open_today = df_today.iloc[0].open
         # 今日最高价
         high_today = df_today.iloc[0].high
         # 今日实时价
         price_today = df_today.iloc[0].price
         # 今日最低价
         low_today = df_today.iloc[0].low
+        # 
         if (
             #★★★★★★★★★★★★★★★★尾盘(2.30以后)选股条件★★★★★★★★★★★★★★★★
-            # 实时选股条件:（买入日，观望日）低于前高，T型
-            # 低于前高
-            # float(high_today) < front_1.high
+            # 实时选股条件:涨幅 > 4.5%
+            float(price_today) / float(open_today) > 1.045
             # T型
-            (float(high_today) - float(price_today)) / (float(high_today) - float(low_today)) < 0.2
+            and (float(high_today) - float(price_today)) / (float(high_today) - float(low_today)) < 0.15
             #★★★★★★★★★★★★★★★★尾盘(2.30以后)选股条件★★★★★★★★★★★★★★★★
             ):
                 print("%06d"%stock_code)  # 股票代码
@@ -49,7 +51,7 @@ def print_up_stock( stock_code ):
 
 
 #读取[Guogao_1days_T_search.py -> YYYYMMDD_T_Guogao1.csv]结果的所有股票代码
-df_stock_codes = pd.DataFrame(pd.read_csv(stock_data_path + var_date +'_T_1.csv', index_col=None))
+df_stock_codes = pd.DataFrame(pd.read_csv(stock_data_path + var_date +'_V.csv', index_col=None))
 
 # 多线程实行
 threads = []
--- src/setInitValue.py
+++ src/setInitValue.py
@@ -6,6 +6,6 @@ import globalvar as gl
 
 gl._init()
 
-gl.set_value('var_date', '20190215')
+gl.set_value('var_date', '20190220')
 gl.set_value('stock_data_path', 'C:/stock_data/')
 gl.set_value('df_all_code_file', 'all_code.csv')
