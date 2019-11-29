0732a23e069df3064e99ea4104423da635fa27b0
Shawn.Wang <javenonly@163.com>
2019-11-29 09:34:47 +0800

    提高效率

--- src/F_过高_下跌1-3_高开_watch.py
+++ src/F_过高_下跌1-3_高开_watch.py
@@ -23,25 +23,60 @@ df_all_code_file = gl.get_value('df_all_code_file')
 #读取[Guogao_1days_T_search.py -> YYYYMMDD_Guogao_n_output.csv]结果的所有股票代码
 df_stock_codes = pd.DataFrame(pd.read_csv(stock_data_path + var_date +'_U_D_H_output.csv', index_col=None))
 
+watch_stock_list = []
+index = 0
+for stock_code in df_stock_codes.code:
+    max_high_value = df_stock_codes.iloc[index].max_high_value
+    # 获取股票实时数据
+    df_today = ts.get_realtime_quotes("%06d"%stock_code)
+    index += 1
+
+    try:
+        # 今日开盘价
+        open_today = df_today.iloc[0].open
+            # 开盘价 > 昨日最高价
+        if (float(open_today) > float(max_high_value)):
+            watch_stock = {'stock_code':"%06d"%stock_code,'stock_value':max_high_value}
+            watch_stock_list.append(watch_stock)
+
+    except IndexError:
+        continue
+        # print("%06d" % stock_code + ':IndexError')
+    except FileNotFoundError:
+        continue
+        # print("%06d" % stock_code + ':FileNotFoundError')
+    except urllib.error.URLError:
+        continue
+        # print("%06d" % stock_code + ':urllib.error.URLError')
+    except socket.timeout:
+        continue
+        # print("%06d" % stock_code + ':socket.timeout')
+    except ZeroDivisionError:
+        continue
+        # print("%06d" % stock_code + ':ZeroDivisionError')
+    except AssertionError:
+        continue
+
 existCode_array = []
 
 while True:
 
-    print("---《N模型-最近7天过高》《今日接近前高(0.98-1.015)..T型》-----------")
+    print("---《最近7天过高》下跌1-3天，今日高开，下跌，回升超过开盘价-----------")
     # 循环抽出的股票代码
     loop_index = 0
-    for stock_code in df_stock_codes.code:
-
-        if len(existCode_array) > 0 and ("%06d"%stock_code in existCode_array) :
+    for stock_item in watch_stock_list:
+        stock_item_code = stock_item['stock_code']
+        stock_item_value = stock_item['stock_value']
+        if len(existCode_array) > 0 and (stock_item_code in existCode_array) :
             loop_index += 1
             continue
 
-        max_high_value = df_stock_codes.iloc[loop_index].max_high_value
+        # max_high_value = stock_item['stock_value']
         loop_index += 1
 
         try:
             # 获取股票实时数据
-            df_today = ts.get_realtime_quotes("%06d"%stock_code)
+            df_today = ts.get_realtime_quotes(stock_item_code)
             # 今日开盘价
             open_today = df_today.iloc[0].open
             # 今日最高价
@@ -52,15 +87,15 @@ while True:
             low_today = df_today.iloc[0].low
             if (
                 # 开盘价 > 昨日最高价
-                float(open_today) > float(max_high_value)
+                float(open_today) > float(stock_item_value)
                 # 今日最低价 < 今日开盘价
                 and float(low_today) < float(open_today)
                 # 今日实时价 > 今日开盘价
                 and float(price_today) >= float(open_today)
                 ):
-                    existCode_array.append("%06d"%stock_code)
+                    existCode_array.append(stock_item_code)
                     # print("%06d"%stock_code)  # 股票代码
-                    print("%06d"%stock_code,":",open_today, ":", time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))  # 股票代码
+                    print(stock_item_code,":",open_today, ":", time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))  # 股票代码
                     # tkinter.messagebox.showinfo('过高提示', '股票：[' + "%06d"%stock_code + ']->过高提示')
 
         except IndexError:
